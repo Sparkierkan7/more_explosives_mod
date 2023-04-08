83cdb06ab09d9af6a7a0af47a541941f81ee6e46
sparkierkan7 <sparkierkan7@gmail.com>
2023-04-08 12:08:12 -0700

    forgot some stuff

--- src/main/java/morefirework/mod/item/BeeswaxFireStarterItem.kt
+++ src/main/java/morefirework/mod/item/BeeswaxFireStarterItem.kt
@@ -71,6 +71,21 @@ class BeeswaxFireStarterItem : Item {
 
     }
 
+    override fun onCraft(stack: ItemStack?, world: World?, player: PlayerEntity?) {
+
+        if (stack?.nbt?.getBoolean("tooltip_nbt") == null || stack?.nbt?.getBoolean("tooltip_nbt") == true) {
+
+            var nbt = NbtCompound()
+            nbt.putInt("durability", 64)
+            nbt.putBoolean("tooltip_nbt", true)
+
+            stack?.setNbt(nbt)
+
+        }
+
+        super.onCraft(stack, world, player)
+    }
+
     override fun appendTooltip(
         stack: ItemStack?,
         world: World?,
