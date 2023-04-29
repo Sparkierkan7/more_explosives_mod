f30b025cd42bb2f92b1edeedb59e7706fcb8f9e3
Sparkierkan7 <87618720+Sparkierkan7@users.noreply.github.com>
2023-04-29 16:54:26 -0700

    Update MorefireworkClient.kt

--- src/main/java/morefirework/mod/MorefireworkClient.kt
+++ src/main/java/morefirework/mod/MorefireworkClient.kt
@@ -24,9 +24,6 @@ class MorefireworkClient : ClientModInitializer {
 
         var LOGGER = LoggerFactory.getLogger("morefirework")
 
-        var airJumpKey: KeyBinding? = null
-        var dashKey: KeyBinding? = null
-
     }
 
     override fun onInitializeClient() {
@@ -40,56 +37,6 @@ class MorefireworkClient : ClientModInitializer {
         EntityRendererRegistry.register(GunpowderShrapnelStickEntityType, ::GunpowderShrapnelStickProjectileRenderer)
         EntityRendererRegistry.register(IronShotEntityType, ::IronShotProjectileRenderer)
         EntityRendererRegistry.register(MusketShotEntityType, ::MusketShotProjectileRenderer)
-
-        //LOGGER.info("client init")
-
-        /*airJumpKey = KeyBindingHelper.registerKeyBinding(KeyBinding(
-
-            "Air Jump",  // The translation key of the keybinding's name
-            InputUtil.Type.KEYSYM,  // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
-            GLFW.GLFW_KEY_R,  // The keycode of the key
-            "Secret hacks" // The translation key of the keybinding's category.
-
-        ))
-
-        dashKey = KeyBindingHelper.registerKeyBinding(KeyBinding(
-
-            "Dash",  // The translation key of the keybinding's name
-            InputUtil.Type.KEYSYM,  // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
-            GLFW.GLFW_KEY_R,  // The keycode of the key
-            "Secret hacks" // The translation key of the keybinding's category.
-
-        ))*/
-
-        ClientTickEvents.END_CLIENT_TICK.register(ClientTickEvents.EndTick { client: MinecraftClient ->
-
-            /*while (airJumpKey!!.wasPressed()) {
-
-                var player = MinecraftClient.getInstance().player!!
-
-                var velocity = player.velocity
-                player.setVelocity(velocity.x, 1.5, velocity.z)
-
-                //client.player!!.sendMessage(Text.literal("YEET ${player.velocity.y}"), false)
-
-            }
-
-            while (dashKey!!.wasPressed()) {
-
-                var nethandler: ClientPlayNetworkHandler = MinecraftClient.getInstance().networkHandler!!
-                var player = MinecraftClient.getInstance().player!!
-
-                //nethandler.sendPacket(PlayerInteractEntityC2SPacket.attack(player, player.isSneaking()))
-
-                var dashv = player.velocity
-
-                dashv = setShootVelocity(player.pitch, player.yaw, 0f, 1.5)
-
-                player.setVelocity(dashv)
-
-            }*/
-
-        })
 
     }
 
